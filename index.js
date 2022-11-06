171c52203c337cd85834ae5fce6e6061451e4054
MarlonHersal <hern.sal.dz@gmail.com>
2022-11-06 16:07:06 -0600

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const {errorHandler, boomErrorHandler} = require("./middleware/error.handler.js");
 const routeApi = require('./routes');
+const cors = require('cors')
 
 const app = express()
-const port = 3000;
+const port = process.env.port || 3000;
+
+const whiteList = ['http://localhost:550', 'http://myapp.co']
+const options = {
+    origin: (origin, callback)=>{
+        if(whiteList.includes(origin) || !origin)
+        {
+            callback(null, true)
+        }
+        else{
+            callback(new Error('no permitido'))
+        }
+    }
+}
 
 app.use(express.json())
+app.use(cors(options))
 
 app.get('/', function(req,res){
     res.send('Esta es la API de una tienda Online')
@@ -14,8 +29,6 @@ app.get('/', function(req,res){
 routeApi(app);
 app.use(boomErrorHandler)
 app.use(errorHandler) 
-
-
 
 app.listen(port, ()=>{
     console.log("Funciona")
